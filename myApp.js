387072a8c9de921f2b816d096e0dff30af4453f7
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-05-14 16:17:11 -0700

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -4,11 +4,17 @@ const app = express();
 console.log('Hello World');
 
 
+
+
+app.get('/', function(req, res) {
+    const path = __dirname + '/views/index.html'
+    res.sendFile(path)
+}) 
+
 app.get("/", function(req, res) {
     res.send('Hello Express')
     } 
 )
-
 
 
 module.exports = app;
